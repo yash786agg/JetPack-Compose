ecd30e6b7596679ad8fc2c819a69242ef8b63643
Yash Agarwal <yash.agarwal@digia.com>
2020-03-06 19:01:49 +0200

    - Implementation of Default and Contained Button Style inside Button.kt JetPack Compose.

--- app/src/main/java/com/app/jetpackcompose/ButtonActivity.kt
+++ app/src/main/java/com/app/jetpackcompose/ButtonActivity.kt
@@ -11,10 +11,13 @@ import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.layout.Column
+import androidx.ui.layout.HeightSpacer
 import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
-import com.app.uiComponents.OutlineButtonStyle
+import com.app.uiComponents.ButtonContainedStyle
+import com.app.uiComponents.ButtonDefaultStyle
+import com.app.uiComponents.ButtonOutlineStyle
 
 class ButtonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,10 +33,33 @@ fun MyButtonView() = MaterialTheme {
     val context = +ambient(ContextAmbient)
     VerticalScroller(isScrollable = true) {
         Column(Spacing(20.dp)) {
-            OutlineButtonStyle(
+            ButtonDefaultStyle(
                 text = context.resources.getString(R.string.app_name),
-                outLineColor = ContextCompat.getColor(context,R.color.colorRed),
-                outLineWidth = 2,
+                strokeColor = ContextCompat.getColor(context,R.color.colorBlack),
+                strokeWidth = 2,
+                roundCorner = 30,
+                rippleColor = ContextCompat.getColor(context,R.color.colorPrimary),
+                buttonColor = ContextCompat.getColor(context,R.color.colorWhite),
+                textColor = ContextCompat.getColor(context,R.color.colorRed),
+                backgroundColor = ContextCompat.getColor(context,R.color.colorYellow),
+                fountType = "berninasans_bold.ttf"
+            )
+
+            HeightSpacer(height = 20.dp)
+            
+            ButtonContainedStyle(
+                text = context.resources.getString(R.string.app_name),
+                rippleColor = ContextCompat.getColor(context,R.color.colorPrimary),
+                buttonColor = ContextCompat.getColor(context,R.color.colorYellow),
+                roundCorner = 20
+            )
+
+            HeightSpacer(height = 20.dp)
+
+            ButtonOutlineStyle(
+                text = context.resources.getString(R.string.app_name),
+                strokeColor = ContextCompat.getColor(context,R.color.colorRed),
+                strokeWidth = 2,
                 elevation = 16,
                 textColor = ContextCompat.getColor(context,R.color.colorBlack),
                 buttonColor = ContextCompat.getColor(context,R.color.colorWhite)
